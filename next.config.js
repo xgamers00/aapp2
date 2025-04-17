118f45e745f58ece2268272c492f5d259785bd2e
xgamers00 <60927927+xgamers00@users.noreply.github.com>
2025-04-17 22:40:49 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,9 +11,9 @@ const nextConfig = {
   },
   env: {
     // API_PROD_URL: "http://127.0.0.1:8000/api/",
-    API_PROD_URL: "https://laravel.pixelstrap.net/fastkart/api/",
-    PAYMENT_RETURN_URL: "http://localhost:3000",
-    PAYMENT_CANCEL_URL: "http://localhost:3000/",
+    API_PROD_URL: "https://",
+    PAYMENT_RETURN_URL: "http://",
+    PAYMENT_CANCEL_URL: "http://",
   },
   images: {
     remotePatterns: [
